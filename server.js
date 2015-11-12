ac078f0d061b5af3d5bc564e67898a9bd44997cd
Elliot Alderton <ellalderton@gmail.com>
2015-11-12 21:17:41 +0000

    Convert GET /todos to sequelize

--- server.js
+++ server.js
@@ -16,25 +16,45 @@ app.get('/', function(req, res) {
 
 // GET /todos?completed=true&q=house
 app.get('/todos', function(req, res) {
-	var queryParams = req.query;
-	var filteredTodos = todos;
+	var query = req.query;
+	var where = {};
 	
-	// if has property && completed === 'true'
-	// filteredTodos =  _.where(filteredTodos, {'completed': true})
-	// else if has property && completed === 'false'
-	// filteredTodos =  _.where(filteredTodos, {'completed': false})
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {completed: true});
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {completed: false});
-	}
-
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		// _.filter - Looks through each value in the list, returning an array of all the values that pass a truth test
-		filteredTodos = _.filter(filteredTodos, function(todo) { return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;});
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed = true;	
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+		where.completed = false;		
 	}
 	
-	res.json(filteredTodos);
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like: `%${query.q}%`
+		};
+	}
+	
+	db.todo.findAll({where: where}).then(function(todos) {
+			res.json(todos);
+	}, function(e) {
+		res.status(500).send();
+	});
+	
+// 	var filteredTodos = todos;
+// 	
+// 	// if has property && completed === 'true'
+// 	// filteredTodos =  _.where(filteredTodos, {'completed': true})
+// 	// else if has property && completed === 'false'
+// 	// filteredTodos =  _.where(filteredTodos, {'completed': false})
+// 	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
+// 		filteredTodos = _.where(filteredTodos, {completed: true});
+// 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
+// 		filteredTodos = _.where(filteredTodos, {completed: false});
+// 	}
+// 
+// 	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+// 		// _.filter - Looks through each value in the list, returning an array of all the values that pass a truth test
+// 		filteredTodos = _.filter(filteredTodos, function(todo) { return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;});
+// 	}
+// 	
+// 	res.json(filteredTodos);
 });
 
 // GET /todos/:id
