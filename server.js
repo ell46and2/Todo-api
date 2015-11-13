05980ecb8069577f026835d44caf7c23c069b23b
Elliot Alderton <ellalderton@gmail.com>
2015-11-13 00:20:46 +0000

    Update PUT /todos/:id to use sequelize

--- server.js
+++ server.js
@@ -161,31 +161,37 @@ app.delete('/todos/:id', function(req, res) {
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
-	var validAttributes = {};
+	var attributes = {};
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {id: todoId});
 	
-	if(!matchedTodo) {
-		return res.status(404).send();
-	};
+	if (body.hasOwnProperty('completed')) {
+		attributes.completed = body.completed;
+	} 
 	
-	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
-		validAttributes.completed = body.completed;
-	} else if (body.hasOwnProperty('completed')) {
-		return res.status(400).send();
-	};
-	
-	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0) {
-		validAttributes.description = body.description;
-	} else if (body.hasOwnProperty('description')) {
-		return res.status(400).send();
-	};
+	if(body.hasOwnProperty('description')) {
+		attributes.description = body.description;
+	}
 	
 		// Copy all of the properties in the source objects over to the destination object, and return the destination object.
 		// Objects in JS are passed by reference, so changing matchedTodo also chages the original reference in the todos array.
-		 _.extend(matchedTodo, validAttributes);
+		//  _.extend(matchedTodo, validAttributes);
+		// 
+		// res.json(matchedTodo);
 		
-		res.json(matchedTodo);
+		db.todo.findById(todoId).then(function(todo) {
+			if(todo) {
+				todo.update(attributes).then(function(todo) {
+					res.json(todo.toJSON());
+				}, function(e) {
+				res.status(400).json(e);
+				});
+			} else {
+				res.status(404).send();
+			}
+		}, function() {
+			res.status(500).send();
+		});
+		
 	
 });
 	
